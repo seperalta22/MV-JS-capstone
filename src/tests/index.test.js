9b3a2f61a61c1c99ff883cbb5586d8602cf956b6
Sergio Peralta <seperalta88@gmail.com>
2023-05-24 11:08:40 -0300

    fix linter errors

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,28 +1,28 @@
 import commentsCounter from '../modules/commentsCounter.js';
 
 describe('commentsCounter', () => {
-	beforeEach(() => {
-		const countText = document.createElement('span');
-		countText.id = 'comments-counter';
-		document.body.appendChild(countText);
-	});
+  beforeEach(() => {
+    const countText = document.createElement('span');
+    countText.id = 'comments-counter';
+    document.body.appendChild(countText);
+  });
 
-	afterEach(() => {
-		const countText = document.getElementById('comments-counter');
-		if (countText) {
-			countText.remove();
-		}
-	});
+  afterEach(() => {
+    const countText = document.getElementById('comments-counter');
+    if (countText) {
+      countText.remove();
+    }
+  });
 
-	it('update comment counter successfully', async () => {
-		// Arrange
-		const count = 5;
+  it('update comment counter successfully', async () => {
+    // Arrange
+    const count = 5;
 
-		// Act
-		await commentsCounter(count);
+    // Act
+    await commentsCounter(count);
 
-		// Assert
-		const countText = document.getElementById('comments-counter');
-		expect(countText.innerText).toBe(`Comments (${count})`);
-	});
+    // Assert
+    const countText = document.getElementById('comments-counter');
+    expect(countText.innerText).toBe(`Comments (${count})`);
+  });
 });
