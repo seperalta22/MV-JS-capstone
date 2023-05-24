b20cded7c429f497395d1ffc004fba14784023b1
mutugi-c <mutugichris@gmail.com>
2023-05-24 18:43:16 +0200

    Get number of likes for item from API

---
+++ src/modules/likes.js
@@ -0,0 +1,25 @@
+const likesApi =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/edecTBZJTxSFvT9kueVx/likes';
+
+const getLikes = async () => {
+  try {
+    const response = await fetch(likesApi);
+    const json = await response.json();
+
+    const cards = Array.from(document.querySelectorAll('.card'));
+    const likesCounter = Array.from(
+      document.querySelectorAll('.likes-counter')
+    );
+
+    cards.forEach((card, index) => {
+      const item = json.find((item) => item.item_id === card.id);
+      if (item) {
+        likesCounter[index].innerHTML = `${item.likes} likes`;
+      }
+    });
+  } catch (error) {
+    alert('Error fetching likes:', error);
+  }
+};
+
+
