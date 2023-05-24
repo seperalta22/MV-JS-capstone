c010c943eaf431069abc3ea16541df50ce54e376
Sergio Peralta <seperalta88@gmail.com>
2023-05-24 19:12:37 -0300

    update styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import fetchApiData from './modules/fetchApiData.js';
 import './styles.css';
+import logoImage from './assets/pokemon.svg';
 
+const logo = document.querySelector('.logo');
+logo.src = logoImage;
 fetchApiData();
