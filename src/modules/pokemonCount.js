a372db39e8b58a49960ba393fce48fc61b789ec1
mutugi-c <mutugichris@gmail.com>
2023-05-24 15:38:09 +0200

    Create a pokemon count feature

---
+++ src/modules/pokemonCount.js
@@ -0,0 +1,5 @@
+export const pokemonsCount = () => {
+	const countElement = document.getElementById('pokemon-count');
+	const cardCount = document.querySelectorAll('.card').length;
+	countElement.innerText = `${cardCount} Pokemons`;
+};
